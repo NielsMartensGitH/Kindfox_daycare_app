e4209ad07ab009c2d91476511138b31b56e9a650
Niels <niels_martens@live.nl>
2022-05-11 11:56:42 +0200

    created models with migrations file

---
+++ app/Models/ClientMainUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClientMainUser extends Model
+{
+    use HasFactory;
+}
