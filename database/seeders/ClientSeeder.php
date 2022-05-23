37920047ef81bdfe1dae7b0096d20bd9607a495c
Niels <niels_martens@live.nl>
2022-05-23 10:08:38 +0200

    created seeder for clients table

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Client;
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create('nl_BE');
+
+        for ($i = 0; $i <= 10; $i++) {
+            $firstName = $faker->firstName('male'|'female');
+            $lastName = $faker->lastName();
+            $age = $faker->numberBetween(0, 12);
+            $checked_in = $faker->numberBetween(0, 1);
+
+            Client::create([
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'age' => $age,
+                'checked_in' => $checked_in
+            ]);
+        }
+    }
+}
