af33727cbc363afc0ff6d0bd90c221eddfd4b5a8
RoelPaulussen <91598567+RoelPaulussen@users.noreply.github.com>
2022-06-01 15:22:23 +0200

    intial post and comments WIP

--- resources/views/layouts/MainUserView.blade.php
+++ resources/views/layouts/MainUserView.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link href="{{ asset('assets/css/Main.css') }} " rel="stylesheet">
 <!-- this is the navbar of the messageboard -->
 </head>
@@ -20,11 +21,7 @@
       <div class="col-sm-10 contentarea">
         <!-- our main component with the posts  -->
         @yield('content')
-        <img src="{{asset('assets/img/profielfotoNiels.jpg')}}" alt="" width=1000px>
-        <img src="{{asset('assets/img/profielfotoNiels.jpg')}}" alt="" width=1000px>
-        <img src="{{asset('assets/img/profielfotoNiels.jpg')}}" alt="" width=1000px>
-        <img src="{{asset('assets/img/profielfotoNiels.jpg')}}" alt="" width=1000px>
-        <img src="{{asset('assets/img/profielfotoNiels.jpg')}}" alt="" width=1000px>
+        
       </div>
       <div class="sidebar side-nav col-sm-2">
         <!-- our sidebar on the right of the page -->
--- resources/views/mainuserview.blade.php
+++ resources/views/mainuserview.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.MainUserView')
 
 @section('content')
-    <h1>mainuserview.blade.php</h1>
+    @foreach($Posts as $post)
+        <div class="row justify-content-center my-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-auto">
+                            COMPANY IMG HERE
+                        </div>
+                        <div class="col-sm-10">
+                            <h5 class="card-title">{{$post->name}}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Posted <span>TIME CALC HERE</span><span> ago</span></h6>
+                        </div>
+                    </div>
+                    <p class="card-text">{{$post->message}}</p>
+                    </div>
+
+                    <div class="card-footer text-muted">
+                        <div class="privacy d-flex justify-content-between">
+                            @if($post->is_private == 0)
+                                <p class="m-0">Every parent can see this message</p>
+                            @else
+                                <p class="m-0">Only You can see this message</p>
+                            @endif
+                            <div class="comments" style="display:none">
+                                @foreach($Comments as $comment)
+                                    <p>{{$comment->message}}</p>
+                                @endforeach
+                            </div>
+                        </div> 
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <hr>
+    @endforeach
 @endsection
