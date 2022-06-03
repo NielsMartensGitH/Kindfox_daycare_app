70e9f2918e120085a513417c397b464bd6f8b127
RoelPaulussen <91598567+RoelPaulussen@users.noreply.github.com>
2022-06-03 10:53:54 +0200

    going further with the posts stuff

--- app/Http/Controllers/mainUserController.php
+++ app/Http/Controllers/mainUserController.php
@@ -6,18 +6,25 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Comment;
 use App\Models\Company;
+use App\Models\MainUser;
 
 class mainUserController extends Controller
 {
     //
     public function getPost(){
+        //GET CLIENT INFO WITH LINKED COMPANY
+        $clientinfo = MainUser::with('companies')->where('id', 6 )->get();
+        dd($clientinfo);
         //REPLACE THE CLIENT ID WITH VERIFIED LOGIN ID
-        $posts = Post::where('is_private',0)->where('company_id',3)->orWhere('is_private',1)->where('client_id',6)->get();
-        //NEEDS JOIN TO SEE TO WHICH POST IT IS ASSIGNED
-        $postcomments = Comment::get();
+        $posts = Post::with('companies')->where('is_private',0)->where('company_id',3)->orWhere('is_private',1)->where('client_id',6)->get();
+        
+        //Here we join the comment table with the pivot table to be able to get the post_id
+        $postcomments = Comment::leftJoin('comment_posts', function($join) {
+            $join->on('comments.id', '=', 'comment_posts.comment_id');
+          })->get();
         $companies = Company::get();
         // dd($posts);
         //dd($postcomments);
-        return view('mainuserview',['Posts' => $posts, 'Comments' => $postcomments, 'Companies' => $companies]);
+        return view('mainuserview',['Posts' => $posts, 'Comments' => $postcomments, 'Companies' => $companies, 'User' => $user]);
     }
 }
