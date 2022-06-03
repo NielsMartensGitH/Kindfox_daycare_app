a85450fd8d48c388c8d4a948608941ae978135e4
Satiko <satikoshapy@yahoo.com>
2022-06-03 15:12:53 +0200

    added a layout for children

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,39 @@
 <x-dashboard-layout>
     <x-slot name="content">
-        <h1>CHILDREN</h1>
+    <div class="row">
+    <div class="col-sm-12 header">
+
+        <h1 class=text-center>Children</h1>
+    </div>
+</div>
+<div class="container">
+<div class="row" >
+    <div class="col-md-3 p-0 m-0 card-group" *ngFor="let child of children$">
+        <div class="card" style="width: 18rem;">
+            <img src="../../../assets/img/profielfotoRoos.jpg" class="card-img-top" alt="..."
+            [ngClass]="{'green-border': child.checked_in === 1}"
+            (dblclick)="onDblClick(child)">
+            <h1>Baby</h1>
+            <div class="card-body d-flex justify-content-center">
+                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                    <button type="button" class="btn btn-kindfox-warning" 
+                    data-bs-toggle="modal" 
+                    data-bs-target="#childEditModal"
+                    (click)="passedChild = child">
+                    Edit</button>
+                    <div>
+                        <button type="button" class="btn btn-kindfox-primary"
+                        data-bs-toggle="modal" data-bs-target="#exampleModal"
+                    (click)="childToDiary = child">Diary</button>
+                    
+                    <button type="button" class="btn btn-kindfox-danger"
+                    (click)="onDelete(child)">Delete</button>
+                  </div>
+            </div>
+          </div>
+    </div>
+   
+</div>
+</div>
     </x-slot>
 </x-dasboard-layout>
