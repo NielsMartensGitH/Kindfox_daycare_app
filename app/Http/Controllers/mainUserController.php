6e8a984af73d5b7726be61cb21008b97c7ceb9fc
RoelPaulussen <91598567+RoelPaulussen@users.noreply.github.com>
2022-06-07 13:29:28 +0200

    diarypage redirect working

--- app/Http/Controllers/mainUserController.php
+++ app/Http/Controllers/mainUserController.php
@@ -8,6 +8,7 @@ use App\Models\Comment;
 use App\Models\Company;
 use App\Models\MainUser;
 use App\Models\Client;
+use App\Models\Diary;
 
 class mainUserController extends Controller
 {
@@ -18,8 +19,8 @@ class mainUserController extends Controller
         //GET CLIENT INFO WITH LINKED COMPANY
         $mainUserInfo = MainUser::with('companies')->distinct()->where('id', $MU_id)->get();
         
-        //REPLACE THE CLIENT ID WITH VERIFIED LOGIN ID
-        $posts = Post::with('companies')->where('is_private',0)->orWhere('is_private',1)->where('client_id',Auth()->user()->main_user_id)->get();
+        $posts = Post::with('comments', 'companies')->get();
+        //$posts = Post::with('companies')->where('is_private',0)->orWhere('is_private',1)->where('client_id',Auth()->user()->main_user_id)->get();
         
         //Here we join the comment table with the pivot table to be able to get the post_id
         $postcomments = Comment::leftJoin('comment_posts', function($join) {
@@ -49,6 +50,22 @@ class mainUserController extends Controller
    
     //SINGLE CLIENT STUFF
     public function getDiaries($id){
-      return view('MainUserViewDiary');
+      //get the id of the ain user
+      $MU_id = Auth()->user()->main_user_id;
+      //check if the user has been assigned to a main user
+      if(!is_null($MU_id)){
+        $clients = Client::leftJoin('client_main_users', function($join) {
+          $join->on('clients.id', '=', 'client_main_users.client_id');
+        })->where('main_user_id',Auth()->user()->main_user_id)->get();
+      }
+      else{
+        $clients = null;
+      }
+
+      $diary = Diary::where('client_id',$id)->get();
+      
+      //dd($diary);
+
+      return view('mainuserviewdiary');
     }
 }
--- resources/views/mainuserview.blade.php
+++ resources/views/mainuserview.blade.php
@@ -5,39 +5,71 @@
         @foreach($User as $user)
             @foreach($user->companies as $company)
                 @if($company->id == $post->company_id)
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-auto">
-                                    COMPANY IMG HERE
-                                    {{$company->name}}
-                                </div>
-                                <div class="col-sm-10">
-                                    <h5 class="card-title">{{$post->name}}</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">Posted <span>{{$post->created_at->diffForHumans()}}</span></h6>
-                                </div>
-                            </div>
-                            <p class="card-text">{{$post->message}}</p>
-                            <img src="..." class="card-img-top" alt="...">
-                        </div>
+                    @if(!empty($Clients))
+                        @foreach($Clients as $client)
+                            @if($post->is_private == 0)
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-auto">
+                                                COMPANY IMG HERE
+                                                {{$company->name}}
+                                            </div>
+                                            <div class="col-sm-10">
+                                                <h5 class="card-title">{{$post->name}}</h5>
+                                                <h6 class="card-subtitle mb-2 text-muted">Posted <span>{{$post->created_at->diffForHumans()}}</span></h6>
+                                            </div>
+                                        </div>
+                                        <p class="card-text">{{$post->message}}</p>
+                                        <img src="..." class="card-img-top" alt="...">
+                                    </div>
 
-                        <div class="card-footer text-muted">
-                            <div class="privacy d-flex justify-content-between">
-                                @if($post->is_private == 0)
-                                    <p class="m-0">Every parent can see this message</p>
-                                @else
-                                    <p class="m-0">Only You can see this message</p>
-                                @endif
-                                <div class="comments">
-                                    @foreach($Comments as $comment)
-                                        @if ($comment->post_id == $post->id)
-                                            <p>{{$comment->message}}</p>
-                                        @endif
-                                    @endforeach
+                                    <div class="card-footer text-muted">
+                                        <div class="privacy d-flex justify-content-between">
+                                                <p class="m-0">Every parent can see this message</p>
+                                            <div class="comments">
+                                                @foreach($Comments as $comment)
+                                                    @if ($comment->post_id == $post->id)
+                                                        <p>{{$comment->message}}</p>
+                                                    @endif
+                                                @endforeach
+                                            </div>
+                                        </div> 
+                                    </div>
                                 </div>
-                            </div> 
-                        </div>
-                    </div>
+                            @elseif($post->is_private == 1 && $post->client_id == $client->id)
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-auto">
+                                                COMPANY IMG HERE
+                                                {{$company->name}}
+                                            </div>
+                                            <div class="col-sm-10">
+                                                <h5 class="card-title">{{$post->name}}</h5>
+                                                <h6 class="card-subtitle mb-2 text-muted">Posted <span>{{$post->created_at->diffForHumans()}}</span></h6>
+                                            </div>
+                                        </div>
+                                        <p class="card-text">{{$post->message}}</p>
+                                        <img src="..." class="card-img-top" alt="...">
+                                    </div>
+
+                                    <div class="card-footer text-muted">
+                                        <div class="privacy d-flex justify-content-between">
+                                                <p class="m-0">Only You can see this message</p>
+                                            <div class="comments">
+                                                @foreach($Comments as $comment)
+                                                    @if ($comment->post_id == $post->id)
+                                                        <p>{{$comment->message}}</p>
+                                                    @endif
+                                                @endforeach
+                                            </div>
+                                        </div> 
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    @endif
                 @endif
             @endforeach
         @endforeach
@@ -45,9 +77,9 @@
 @endsection
 
 @section('children')
-    @if(!empty($Clients != NULL))
+    @if(!empty($Clients))
         @foreach ($Clients as $client)
-            <a href=""><i class="fas fa-baby border rounded-circle p-2"></i>{{$client->first_name}} {{$client->last_name}}</a>
+            <a href="{{route('mainuserviewclients',$client->id)}}"><i class="fas fa-baby border rounded-circle p-2"></i>{{$client->first_name}} {{$client->last_name}}</a>
             <br>
         @endforeach
     @endif
