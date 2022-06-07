b9df914cd87f6bde9414e78236ed322350597d72
Satiko <satikoshapy@yahoo.com>
2022-06-07 10:58:23 +0200

    merge

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class DashBoardController extends Controller
 {
+    public function getClients(){
+        $clients = Client::leftJoin('client_main_users', function($join) {
+            $join->on('clients.id', '=', 'client_main_users.client_id');
+          })->where('main_user_id',Auth()->user()->main_user_id)->get();
+        return $this->getPost($clients); 
+    }
     public function index() {
-        return view('dashboard');
+        return view('dashboard', ['Clients' => $clients]);
     }
 
     public function show_parents() {
