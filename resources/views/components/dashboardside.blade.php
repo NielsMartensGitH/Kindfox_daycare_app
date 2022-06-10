734d540e242e7c4e4498bc92be009879018f625c
Niels <niels_martens@live.nl>
2022-06-10 17:31:34 +0200

    profile pictures are always perfect rounded now

--- resources/views/components/dashboardside.blade.php
+++ resources/views/components/dashboardside.blade.php
@@ -34,13 +34,15 @@
         <!-- HERE WE SEE WHICH DAYCARE WE ARE LOGGED IN AS -->
         <div class="sb-sidenav-footer">
             <div class="d-flex justify-content-left flex-gap-2 align-items-center">
-                <div>
-                    <img src="{{Auth::user()->company()->first()->getMedia()[0]->getFullUrl()}}" width="35px" class="mx-2 rounded-circle">
+                <div class="mx-2">
+                    <div class="circular--landscape">
+                        <img src="{{Auth::user()->company()->first()->getMedia()[0]->getFullUrl()}}">
+                    </div>
                 </div>
                 <div>
-                    {{ Auth::user()->name }}
+                    <h4>{{ Auth::user()->name }}</h4>
                 </div>
-            </div>
+                </div>
         </div>
     </nav>
 </div>
