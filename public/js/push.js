9762f542f7523ea95d72028c5aa367b80c2ddcc7
Niels <niels_martens@live.nl>
2022-06-16 14:54:04 +0200

    added migrations for notifications table and notification_users table

--- public/js/push.js
+++ public/js/push.js
@@ -10,11 +10,6 @@ var notificationsToggle    = notificationsWrapper.find('button[data-bs-toggle]')
 var notificationsCountElem = notificationsToggle.find('.badge');
 var notificationsCount     = parseInt(notificationsCountElem[0].innerHTML);
 var notifications          = notificationsWrapper.find('.notification-cards');
-console.log(notifications)
-
-if (notificationsCount <= 0) {
-  notificationsCountElem.hide();
-}
 
 var pusher = new Pusher('8c8ccd2561cb3bb20be8', {
   cluster: 'eu'
@@ -25,15 +20,19 @@ var channel = pusher.subscribe('new-comment');
 
 // Bind a function to a Event (the full Laravel class)
 channel.bind('App\\Events\\NewComment', function(data) {
+  console.log(data);
   var existingNotifications = notifications.html();
   var newNotificationHtml = `
   <div class="d-flex p-4 align-items-center justify-content-between border bg-light">
-                            <div class=""><i class="fas fa-envelope text-danger mx-2"></i><a class="new-message" href="#">1 New Message from daycare</a></div>
-                            <small>2 hours ago</small>
-                        </div>
+    <div class="">
+      <i class="fas fa-envelope text-danger mx-2"></i><a class="new-message" href="#">${data.message}</a>
+    </div>
+    <small>2 hours ago</small>
+  </div>
   `;
   notifications.html(newNotificationHtml + existingNotifications);
 
   notificationsCount += 1;
+
   notificationsCountElem[0].innerHTML = notificationsCount;
 });
